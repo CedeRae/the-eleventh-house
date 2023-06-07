bc54c9bc069132090d9a853439959a311dde2c5d
CedeRae <100053806+CedeRae@users.noreply.github.com>
2023-06-07 11:27:11 -0700

    latest commit

---
+++ js/product.js
@@ -0,0 +1,10 @@
+/*const sizeSelection = document.querySelector(".sizes").querySelectorAll("#size");
+console.log(sizeSelection);
+
+sizeSelection.forEach(element => {
+    element.addEventListener("click", function() {
+        sizeSelection.forEach(el => el.classList.remove("active"))
+
+        this.classList.add("active");
+    })
+});*/
