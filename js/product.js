23012cca51d0478b6e88f2f1676b4e8555615195
CedeRae <100053806+CedeRae@users.noreply.github.com>
2023-11-02 17:08:25 -0700

    added toggle for imgs and sizes

--- js/product.js
+++ js/product.js
@@ -1,10 +1,27 @@
-/*const sizeSelection = document.querySelector(".sizes").querySelectorAll("#size");
-console.log(sizeSelection);
 
-sizeSelection.forEach(element => {
-    element.addEventListener("click", function() {
-        sizeSelection.forEach(el => el.classList.remove("active"))
+//toggle image carousel
+const productImages = document.querySelectorAll(".product-images img");
+const productSlide = document.querySelector(".image-carousel");
 
-        this.classList.add("active");
+let activeSlide = 0;
+
+productImages.forEach((item, i) => {
+    item.addEventListener('click', () => {
+        productImages[activeSlide].classList.remove('active');
+        item.classList.add('active');
+        productSlide.style.backgroundImage = `url('${item.src}')`;
+        activeSlide = i;
     })
-});*/
+});
+
+//toggle size button
+const sizeBtns = document.querySelectorAll('.size-selection');
+let checkedBtn = 0;
+
+sizeBtns.forEach((item, i) => {
+    item.addEventListener('click', () => {
+        sizeBtns[checkedBtn].classList.remove('check');
+        item.classList.add('check');
+        checkedBtn = i;
+    })
+});
